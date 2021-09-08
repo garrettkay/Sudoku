02329bdc6cd4b23fd714351bc7f33747326ca173
garrettkay <garrettkay@gmail.com>
2021-09-08 12:18:08 -0700

    why did it change

--- test.py
+++ test.py
@@ -6,25 +6,26 @@ pygame.display.set_caption("SudokuSolver")
 
 def pygamePrint(text,pos,size,font,color,aa):
 	if text == None:
-		screen.blit(pygame.font.SysFont("Arial",12).render("New text box",False,(0,0,0))),(0,0))
+		screen.blit(pygame.font.SysFont("Arial",12).render("New text box",False,(0,0,0)),(0,0))
 		return
 	if pos == None:
-		screen.blit(pygame.font.SysFont("Arial",12).render(text,False,(0,0,0))),(0,0))
+		screen.blit(pygame.font.SysFont("Arial",12).render(text,False,(0,0,0)),(0,0))
 		return
 	if size == None:
-		screen.blit(pygame.font.SysFont("Arial",12).render(text,False,(0,0,0))),pos
+		screen.blit(pygame.font.SysFont("Arial",12).render(text,False,(0,0,0)),pos)
 		return
 	if font == None:
-		screen.blit(pygame.font.SysFont("Arial",size).render(text,False,(0,0,0))),pos
+		screen.blit(pygame.font.SysFont("Arial",size).render(text,False,(0,0,0)),pos)
 		return
 	if color == None:
-		screen.blit(pygame.font.SysFont(font,size).render(text,False,(0,0,0))),pos
+		screen.blit(pygame.font.SysFont(font,size).render(text,False,(0,0,0)),pos)
 		return
 	if aa == None:
-		screen.blit(pygame.font.SysFont(font,size).render(text,False,color)),pos
+		screen.blit(pygame.font.SysFont(font,size).render(text,False,color),pos)
 		return
 	screen.blit(pygame.font.SysFont(font,size).render(text,aa,color),pos)
 
+pygamePrint("Hello World!",(100,100),24,"Comic Sans MS",(255,0,0),True)
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
